4786fdbb76f74ebca52ac49a085c8199479c81d2
Saba <sabasulakveidze@gmail.com>
2026-03-12 23:27:01 +0400

    feat: modify config

--- Program.cs
+++ Program.cs
@@ -1,14 +1,37 @@
+using FinalProject.Mappers;
+using Microsoft.EntityFrameworkCore;
+using OnlineCourseManagement.Models;
+using OnlineCourseManagement.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<IUsersService, UsersService>();
+
+builder.Services.AddDbContext<OnlineCourseManagementDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(UserMapping));
+
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
