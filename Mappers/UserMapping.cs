346b7e7fb9a12d66d61fd14a5b04f5124b23d75d
Saba <sabasulakveidze@gmail.com>
2026-03-12 23:25:31 +0400

    feat: add mappers

---
+++ Mappers/UserMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FinalProject.Models.Requests;
+using FinalProject.Models.Responses;
+using OnlineCourseManagement.Models;
+
+namespace FinalProject.Mappers
+{
+    public class UserMapping:Profile
+    {
+        public UserMapping()
+        {
+            CreateMap<CreateUserRequest, User>();
+            CreateMap<UpdateUserRequest, User>();
+            CreateMap<User, UserResponse>();
+        }
+    }
+}
