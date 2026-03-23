4597c9a04c5c5103b5f3f5fbc0ca42a4077884ec
Saba <sabasulakveidze@gmail.com>
2026-03-23 23:07:17 +0400

    merge done

---
+++ Migrations/20260323185819_YourMigrationName.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineCourseManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class YourMigrationName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserPassword",
+                table: "Users",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Position",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "(newid())"),
+                    PositionName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Position__3214EC07D9D8112D", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersPosition",
+                columns: table => new
+                {
+                    UsersId = table.Column<int>(type: "int", nullable: false),
+                    PositionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "(newid())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__UsersPos__254209C504877765", x => new { x.UsersId, x.PositionId });
+                    table.ForeignKey(
+                        name: "FK__UsersPosi__Posit__797309D9",
+                        column: x => x.PositionId,
+                        principalTable: "Position",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__UsersPosi__Users__787EE5A0",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersPosition_PositionId",
+                table: "UsersPosition",
+                column: "PositionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersPosition");
+
+            migrationBuilder.DropTable(
+                name: "Position");
+
+            migrationBuilder.DropIndex(
+                name: "UQ_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserPassword",
+                table: "Users");
+        }
+    }
+}
